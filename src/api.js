25a982d5ff1ce22fe2006dab673603186b5107b9
lomaTT <alexeyttdeveloper@gmail.com>
2023-07-06 01:58:36 +0200

    initial commit, first design

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '2bd6d00bbfmsh5d0bead60d811d3p1650a2jsnca2cbe2e8624',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
