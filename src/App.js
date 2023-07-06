b13e6c47510bcaf37391508150d9018f5cc61f36
lomaTT <alexeyttdeveloper@gmail.com>
2023-07-06 16:44:36 +0200

    realization of current weather component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
+import {useState} from "react";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric`)
+    // .then(res => res.json())
+    // .then(res => console.log(res))
+    // .catch(error => console.log(error));
+
+    const forecastFetch = fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric`)
+    // .then(res => res.json())
+    // .then(res => console.log(res))
+    // .catch(error => console.log(error));
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+        setCurrentWeather({city: searchData.label, ...weatherResponse});
+        setForecast({city: searchData.label, ...forecastResponse});
+      })
+      .catch((err) => console.log(err));
   };
+
+  // console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      <Search onSearchChange={handleOnSearchChange}/>
+      {currentWeather && <CurrentWeather data={currentWeather}/>}
     </div>
   );
 }
